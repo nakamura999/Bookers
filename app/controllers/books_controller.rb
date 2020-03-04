0141d83871544eef9a7cfb8d60464e1559916fea
nakamura999 <masahiro9ama9@gmail.com>
2020-03-04 21:24:39 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,34 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to blog_path(book)
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
+
