0a4e4b5a39f6a9ffbf1183d80d01918a09607eb5
nakamura999 <masahiro9ama9@gmail.com>
2020-03-05 18:04:49 +0900

    [Finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
   def update
   	book = Book.find(params[:id])
   	book.update(book_params)
-  	flash[:notice] = "Book was succesfully updated."
+  	flash[:notice] = "Book was successfully updated."
   	redirect_to book_path(book.id)
   end
 
